488e8eb53f02f1a726fa2e8212ea2d3f88f0d2e9
Refound <137168144+Refound-445@users.noreply.github.com>
2024-10-22 18:19:14 +0800

    Add files via upload

--- nonebot_plugin_nailongremove/__init__.py
+++ nonebot_plugin_nailongremove/__init__.py
@@ -6,8 +6,8 @@ from nonebot import get_plugin_config
 from .handler import *
 
 __plugin_meta__ = PluginMetadata(
-    name="NailongRemove",
-    description="一个基于分类模型的简单插件~",
+    name="自动撤回奶龙",
+    description="一个基于图像分类模型的简单插件~",
     usage="只要群内有人发奶龙就会被撤回",
     type="application",
     homepage="https://github.com/Refound-445/onoebot-plugin-nailongremove",
